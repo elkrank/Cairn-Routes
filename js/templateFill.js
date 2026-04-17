791c5311df17f9d89d6943a09aee30fe94c2661f
jamoellm <jamoellm@students.uni-mainz.de>
2026-04-17 21:00:31 +0200

    implemented urlParams instead of localStorage, for persistent links

--- js/templateFill.js
+++ js/templateFill.js
@@ -1,38 +1,39 @@
 async function templateFill() {
-  var routeID = localStorage.getItem("routeID");
-  localStorage.clear();
+  const urlParams = new URLSearchParams(window.location.search);
+  const routeID = urlParams.get("id");
 
   const content = JSON.parse(sessionStorage.getItem("allRoutes"));
+  const route = content[routeID];
 
-  buffs = "";
-  if (content[routeID].buffs['Burst'] === true) {
+  let buffs = "";
+  if (route.buffs['Burst'] === true) {
     buffs += "<img src=\"/imgs/burst.png\" alt=\"Burst buff\">";
   }
-  if (content[routeID].buffs['Grip'] === true) {
+  if (route.buffs['Grip'] === true) {
     buffs += "<img src=\"/imgs/grip.png\" alt=\"Grip buff\">";
   }
-  if (content[routeID].buffs['Focus'] === true) {
+  if (route.buffs['Focus'] === true) {
     buffs += "<img src=\"/imgs/focus.png\" alt=\"Focus buff\">";
   }
-  if (content[routeID].buffs['Chalk'] === true) {
+  if (route.buffs['Chalk'] === true) {
     buffs += "<img src=\"/imgs/extra_grip.png\" alt=\"Chalk bag\">";
   }
-  if (content[routeID].buffs['Grit'] === true) {
+  if (route.buffs['Grit'] === true) {
     buffs += "<img src=\"/imgs/grit.png\" alt=\"Grit buff\">";
   }
   if (buffs === "") {
     buffs += "<img src=\"/imgs/none.png\" alt=\"Crossed through circle\">";
   }
-  document.getElementById('route-title').innerHTML = content[routeID].route_name;
-  document.getElementById('route-name').innerHTML = content[routeID].route_name;
-  document.getElementById('route-location').innerHTML = "<img src=\"/imgs/pin-map.svg\" alt=\"Map pin\"> " + content[routeID].route_location;
-  document.getElementById('route-first-ascent-name').innerHTML = content[routeID].first_ascent_name;
-  document.getElementById('route-first-ascent-date').innerHTML = content[routeID].first_ascent_date;
-  document.getElementById('route-first-redpoint-name').innerHTML = content[routeID].first_redpoint_name;
-  document.getElementById('route-first-redpoint-date').innerHTML = content[routeID].first_redpoint_date;
+  document.getElementById('route-title').innerHTML = route.route_name;
+  document.getElementById('route-name').innerHTML = route.route_name;
+  document.getElementById('route-location').innerHTML = "<img src=\"/imgs/pin-map.svg\" alt=\"Map pin\"> " + route.route_location;
+  document.getElementById('route-first-ascent-name').innerHTML = route.first_ascent_name;
+  document.getElementById('route-first-ascent-date').innerHTML = route.first_ascent_date;
+  document.getElementById('route-first-redpoint-name').innerHTML = route.first_redpoint_name;
+  document.getElementById('route-first-redpoint-date').innerHTML = route.first_redpoint_date;
   document.getElementById('route-buffs-used').innerHTML = buffs;
-  document.getElementById('route-description').innerHTML = content[routeID].route_description;
-  document.getElementById('route-image').innerHTML = "<img src=\"" + content[routeID].img_path + "\" class=\"img-fluid rounded\"></img>";
-  document.getElementById('route-first-ascent-video').innerHTML = "<iframe width=\"560\" height=\"315\" src=\"" + content[routeID].first_ascent_video_url + "\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>";
-  document.getElementById('route-first-redpoint-video').innerHTML = "<iframe width=\"560\" height=\"315\" src=\"" + content[routeID].first_redpoint_video_url + "\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>";
+  document.getElementById('route-description').innerHTML = route.route_description;
+  document.getElementById('route-image').innerHTML = "<img src=\"" + route.img_path + "\" class=\"img-fluid rounded\"></img>";
+  document.getElementById('route-first-ascent-video').innerHTML =   "<iframe width=\"560\" height=\"315\" src=\"" + route.first_ascent_video_url   + "\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>";
+  document.getElementById('route-first-redpoint-video').innerHTML = "<iframe width=\"560\" height=\"315\" src=\"" + route.first_redpoint_video_url + "\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>";
 }
